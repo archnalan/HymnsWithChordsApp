a13bc4f4c14d457b81cd2435d716c5a9da90ebd8
archnalan <archnalan@gmail.com>
2024-04-08 10:28:11 +0300

    Add project files.

---
+++ HymnsWithChords/Models/Hymn.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HymnsWithChords.Models
+{
+	public class Hymn
+	{
+		[Key]
+		public int Id { get; set; }
+
+		[Required]
+		public int Number { get; set; }
+
+		[Required]
+		[StringLength(100)]
+		public string Title { get; set; }
+
+		[NotMapped]
+		public IFormFile? TextUpload { get; set; }
+        public string? WrittenDateRange { get; set; }
+        public string? WrittenBy { get; set; }
+        public string? History { get; set; }
+        public string? AddedBy { get; set; }
+		public DateTime AddedDate { get; set; }
+        public int CategoryId { get; set; }       
+
+        [ForeignKey(nameof(CategoryId))]
+        public virtual Category Category { get; set; }
+
+        public virtual ICollection<LyricSegment> LyricSegments { get; set; }
+
+    }
+}
---
+++ HymnsWithChords/Models/LyricSegment.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HymnsWithChords.Models
+{
+	public class LyricSegment
+	{
+		[Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Words { get; set; }
+
+		[StringLength(10)]
+		[RegularExpression(@"^([A-G])(#|b|##|bb)?(\d+|m|maj|min|sus|aug|dim|add)?(/A-G?)?$", 
+			ErrorMessage = "Invalid Chord Format!")]
+		public string Chord { get; set; }
+
+		[Required]
+        public int HymnId { get; set; }
+
+        [ForeignKey(nameof(HymnId))]
+        public virtual Hymn Hymn { get; set; }
+    }
+}
